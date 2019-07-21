21fb61740ec9ded599cc841400bb840bf1bb96ef
Lara <lara.durrant@gmail.com>
2019-07-21 09:54:33 -0700

    Create SimpleBreakers game
    This is a watered down version of the brick breaker game for learning purposes

---
+++ Assets/ballMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour {
+    Transform ball_transform;
+    float y_speed = -0.1f;
+    float x_speed = 0.1f;
+
+
+
+    // Use this for initialization
+    void Start () {
+        ball_transform = this.gameObject.transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 coords;
+        coords = ball_transform.position;
+
+        coords.y += y_speed;
+        coords.x += x_speed;
+
+        ball_transform.position = coords;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "RightEdge")
+        {
+            Debug.Log("collision detected right");
+            x_speed = x_speed  * -1;
+        }
+
+        if (collision.gameObject.name == "TopEdge")
+        {
+            Debug.Log("collision detected top");
+            y_speed = y_speed * -1;
+        }
+
+        if (collision.gameObject.name == "BottomEdge")
+        {
+            Debug.Log("collision detected bottom");
+            y_speed = y_speed * -1;
+
+        }
+
+        if (collision.gameObject.name == "LeftEdge")
+        {
+            Debug.Log("collision detected left");
+
+            x_speed = x_speed * -1;
+        }
+
+        if(collision.gameObject.name == "Paddle")
+        {
+            y_speed = y_speed * -1;
+        }
+
+        if(collision.gameObject.tag == "Breakable")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
---
+++ Assets/paddle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour {
+
+    public GameObject my_paddle;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left arrow pressed");
+            Vector3 coords;
+            coords = my_paddle.gameObject.transform.position;
+            if (coords.x > -6.0f)
+            {
+                coords.x -= 0.1f;
+            }
+            my_paddle.transform.position = coords;
+      
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Right arrow pressed");
+            Vector3 coords;
+            Debug.Log("Left arrow pressed");
+            coords = my_paddle.gameObject.transform.position;
+            if (coords.x < 4.5f)
+            {
+                coords.x += 0.1f;
+            }
+            my_paddle.transform.position = coords;
+            Debug.Log(coords.x);
+        }
+	}
+}
